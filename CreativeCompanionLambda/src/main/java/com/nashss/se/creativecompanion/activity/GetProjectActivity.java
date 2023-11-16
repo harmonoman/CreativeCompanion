7462488bb2e255cdb630585a32ec8c6ffc8e79fa
Mark Bond <markbond615@gmail.com>
2023-11-15 22:15:43 -0600

    built classes/test for GetProject vertical

---
+++ CreativeCompanionLambda/src/main/java/com/nashss/se/creativecompanion/activity/GetProjectActivity.java
@@ -0,0 +1,54 @@
+package com.nashss.se.creativecompanion.activity;
+
+
+
+import com.nashss.se.creativecompanion.dynamodb.ProjectDao;
+import com.nashss.se.creativecompanion.dynamodb.models.ModelConverter;
+import com.nashss.se.creativecompanion.dynamodb.models.Project;
+import com.nashss.se.creativecompanion.dynamodb.models.ProjectModel;
+import com.nashss.se.creativecompanion.requests.GetProjectRequest;
+import com.nashss.se.creativecompanion.results.GetProjectResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+
+/**
+ * Implementation of the GetProjectActivity for the ProjectService's GetProject API.
+ * <p>
+ * This API allows the customer to see the details of a specfic project.
+ */
+public class GetProjectActivity {
+
+    private final Logger log = LogManager.getLogger();
+    private final ProjectDao projectDao;
+
+    /**
+     * Instantiates a new GetProjectActivity object.
+     *
+     * @param projectDao ProjectDao to access the project table.
+     */
+    @Inject
+    public GetProjectActivity(ProjectDao projectDao) {
+        this.projectDao = projectDao;
+    }
+
+    /**
+     * This method handles the incoming request by retrieving a specified user's Project from the database.
+     * <p>
+     * It then returns the matching project.
+     *
+     * @param getProjectRequest request object containing the User ID & the Project Id
+     * @return getProjectResult result object containing the project requested that were created by that User ID
+     */
+    public GetProjectResult handleRequest(final GetProjectRequest getProjectRequest) {
+        log.info("Received GetProjectRequest {}", getProjectRequest);
+
+        Project result = projectDao.getProject(getProjectRequest.getUserId(), getProjectRequest.getProjectId());
+        ProjectModel projectModel = new ModelConverter().toProjectModel(result);
+
+        return GetProjectResult.builder()
+                .withProject(projectModel)
+                .build();
+    }
+}
---
+++ CreativeCompanionLambda/src/test/java/com/nashss/se/creativecompanion/GetProjectActivityTest.java
@@ -0,0 +1,67 @@
+package com.nashss.se.creativecompanion;
+
+import com.google.common.collect.Sets;
+import com.nashss.se.creativecompanion.activity.GetProjectActivity;
+import com.nashss.se.creativecompanion.dynamodb.ProjectDao;
+import com.nashss.se.creativecompanion.dynamodb.models.Project;
+import com.nashss.se.creativecompanion.requests.GetProjectRequest;
+import com.nashss.se.creativecompanion.results.GetProjectResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class GetProjectActivityTest {
+
+    @Mock
+    private ProjectDao projectDao;
+
+    private GetProjectActivity getProjectActivity;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        getProjectActivity = new GetProjectActivity(projectDao);
+    }
+
+    @Test
+    public void handleRequest_savedProjectFound_returnsProjectModelInResult() {
+        // GIVEN
+        String expectedUserId = "expectedUserId";
+        String expectedProjectId = "expectedProjectId";
+        String expectedProjectName = "expectedProjectName";
+        List<String> expectedWordPool = List.of("words");
+        List<String> expectedWorkspace = List.of("workspace");
+
+
+        Project project = new Project();
+        project.setUserId(expectedUserId);
+        project.setProjectId(expectedProjectId);
+        project.setProjectName(expectedProjectName);
+        project.setWordPool(expectedWordPool);
+        project.setWorkspace(expectedWorkspace);
+
+        when(projectDao.getProject(expectedUserId, expectedProjectId)).thenReturn(project);
+
+        GetProjectRequest request = GetProjectRequest.builder()
+                .withUserId(expectedUserId)
+                .withProjectId(expectedProjectId)
+                .build();
+
+        // WHEN
+        GetProjectResult result = getProjectActivity.handleRequest(request);
+
+        // THEN
+        assertEquals(expectedUserId, result.getProject().getUserId());
+        assertEquals(expectedProjectId, result.getProject().getProjectId());
+        assertEquals(expectedProjectName, result.getProject().getProjectName());
+        assertEquals(expectedWordPool, result.getProject().getWordPool());
+        assertEquals(expectedWorkspace, result.getProject().getWorkspace());
+    }
+}
