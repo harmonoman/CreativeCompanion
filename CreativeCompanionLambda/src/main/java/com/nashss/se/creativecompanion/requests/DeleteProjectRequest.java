c3f2fc5ff07cf7678876a932cbb8739e4a737945
Mark Bond <markbond615@gmail.com>
2023-11-21 11:26:00 -0600

    built classes and tests for DeleteProject vertical

---
+++ CreativeCompanionLambda/src/main/java/com/nashss/se/creativecompanion/requests/DeleteProjectRequest.java
@@ -0,0 +1,51 @@
+package com.nashss.se.creativecompanion.requests;
+
+public class DeleteProjectRequest {
+
+    private final String userId;
+    private final String projectId;
+
+    private DeleteProjectRequest(String userId, String projectId) {
+        this.userId = userId;
+        this.projectId = projectId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    @Override
+    public String toString() {
+        return "DeleteProjectRequest{" +
+                "userId='" + userId + '\'' +
+                ", projectId='" + projectId + '\'' +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String userId;
+        private String projectId;
+
+        public Builder withUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder withProjectId(String projectId) {
+            this.projectId = projectId;
+            return this;
+        }
+
+        public DeleteProjectRequest build() {
+            return new DeleteProjectRequest(userId, projectId);
+        }
+    }
+}
