4c3a4560c8466f527d7cde22e5f3da76615b2cab
harmonoman <markbond615@gmail.com>
2023-12-23 19:36:33 -0600

    fixed import order checkstyles

--- CreativeCompanionLambda/src/main/java/com/nashss/se/creativecompanion/activity/GetWordPoolListActivity.java
+++ CreativeCompanionLambda/src/main/java/com/nashss/se/creativecompanion/activity/GetWordPoolListActivity.java
@@ -1,11 +1,11 @@
 package com.nashss.se.creativecompanion.activity;
 
+import com.nashss.se.creativecompanion.activity.request.GetWordPoolListRequest;
+import com.nashss.se.creativecompanion.activity.result.GetWordPoolListResult;
 import com.nashss.se.creativecompanion.converters.ModelConverter;
 import com.nashss.se.creativecompanion.dynamodb.WordPoolDao;
 import com.nashss.se.creativecompanion.dynamodb.models.WordPool;
 import com.nashss.se.creativecompanion.models.WordPoolModel;
-import com.nashss.se.creativecompanion.activity.request.GetWordPoolListRequest;
-import com.nashss.se.creativecompanion.activity.result.GetWordPoolListResult;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
